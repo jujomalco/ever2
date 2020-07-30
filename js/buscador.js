81bf13d1b524402965403f1d090f272d50eb9e3c
nadojuan <juanmaldonado0015@gmail.com>
2020-07-30 15:08:33 -0400

    index terminado

--- js/buscador.js
+++ js/buscador.js
@@ -1,9 +1,20 @@
 (function(){ 
     $("#qDesea").on("change", function() {
-        $('#qCiudad').removeAttr('hidden');
+        $('#qEspecialidad').removeAttr('hidden');
         $('#qDesea').removeClass('w-40');
         $('#qDesea').addClass('w-25');
-        $('.has-feedback').removeClass('w-40');
-        $('.has-feedback').addClass('w-25');
+        $('.input-buscador').removeClass('w-40');
+        $('.input-buscador').addClass('w-25');
+    })
+ })();
+(function(){ 
+    $("#qEspecialidad").on("change", function() {
+        $('#qCiudad').removeAttr('hidden');
+        $('#qDesea').removeClass('w-25');
+        $('#qDesea').addClass('w-20');
+        $('#qEspecialidad').removeClass('w-25');
+        $('#qEspecialidad').addClass('w-20');
+        $('.input-buscador').removeClass('w-25');
+        $('.input-buscador').addClass('w-20');
     })
  })();
